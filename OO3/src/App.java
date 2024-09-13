8b83745fed80d8a8adb27567e0408869f6097ce0
WattRushberry <Daveniewata1132@gmail.com>
2024-09-13 10:56:05 +0700

    He ehee eheeeee

--- OO3/src/App.java
+++ OO3/src/App.java
@@ -1,14 +1,14 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        Member platinum = new Platinum("Watt", 1000);
+        Member platinum = new Platinum("Son", 1000);
         System.out.println("Platinum info: ");
         platinum.display();
 
-        Member gold = new Gold("Watt", 1652);
+        Member gold = new Gold("Oat", 1652);
         System.out.println("Gold info: ");
         gold.display();
 
-        Member silver = new Silvy("Watt", 1200);
+        Member silver = new Silvy("Fill", 1200);
         System.out.println("Silver info: ");
         silver.display();
     }
